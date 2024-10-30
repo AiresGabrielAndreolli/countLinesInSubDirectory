b8d007005b74dafd53e6a6e23c25ae39c65e8509
Gabriel Aires <80930072+AiresGabrielAndreolli@users.noreply.github.com>
2024-10-30 00:55:51 -0300

    Adicionar arquivos de projeto.

---
+++ arquivoCSV/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+    internal class Program
+    {
+
+
+        static void listaDiretorio(string diretorio)
+        {
+
+
+            foreach (string files in Directory.GetFiles(diretorio, "*.html"))
+            {
+                Console.WriteLine(files);
+                Console.WriteLine(countLinha(files));
+
+            }
+
+
+            foreach (string dir in Directory.GetDirectories(diretorio))
+            {
+ //               Console.WriteLine(dir);
+                listaDiretorio(dir);
+            }
+
+        }
+
+        static int countLinha(string caminho)
+        {
+            int cont = 0;
+            string line;
+            StreamReader sr = new StreamReader(caminho);
+            //Read the first line of text
+            line = sr.ReadLine();
+            //Continue to read until you reach end of file
+            while (line != null)
+            {
+                line = sr.ReadLine();
+                
+                cont++;
+            }
+            //close the file
+            sr.Close();
+
+            return cont;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string caminho = @"..\";
+            listaDiretorio(caminho);
+            Console.ReadLine();   
+        }
+    }
+}
